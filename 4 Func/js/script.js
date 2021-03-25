7a365bac7e9ba414180c358ab116d5668534473b
SkayPC <worldskaynet@gmail.com>
2021-03-25 18:25:31 +0700

    4 Func

---
+++ 4 Func/js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// let a = 2;
+// let c = 4;
+// let test = 20;
+
+// function showFirstMessage(a, c) {
+//   console.log("Привет мир - ", a + c);
+//   test = 11;
+// }
+
+// showFirstMessage(5, 10);
+// showFirstMessage(1, 10);
+// showFirstMessage(2, 10);
+// showFirstMessage(3, 10);
+
+
+let str = '1234 YouTube';
+const a = 'Стало';
+
+// console.log('Было ', str);
+// console.log(a, str.toLowerCase());
+// console.log(a, str.toUpperCase());
+// console.log(a, str.indexOf('Y'));
+// // console.log(a, str.slice(5, 12));
+// console.log(a, str.substring(5, 12));
+
+// const num = 10.90;
+// console.log(Math.round(num));
+const num = "5.6px";
+
+console.log(parseInt(str));
+console.log(parseInt(num));
+console.log(parseFloat(str));
+console.log(parseFloat(num));
+
+
+
