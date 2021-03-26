d60444b85a2103e61cf4d07be0d9b82c3d7a9141
SkayPC <worldskaynet@gmail.com>
2021-03-26 16:18:44 +0700

    Sort and Copy OBJ & Spread ...

---
+++ 7 Sort and Copy OBJ/js/script.js
@@ -0,0 +1,158 @@
+"use strict";
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 28, 12, 'aaa', 'vvvv'];
+
+// arr.sort();
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//   return a - b;
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products.join(' |-_-| '));
+
+// arr.forEach(function(item, i, arr) {
+//   console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// console.log(str);
+
+// console.log(arr.length);
+
+
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+// console.log(arr);
+// arr.push(11);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+
+
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(a);
+// console.log(b);
+
+// const obj = {
+//   a: 5,
+//   b: 2
+// };
+
+// const copy = obj;
+// copy.a = b;
+
+// console.log(copy);
+// console.log(obj);
+
+
+// function copy(mainObj) {
+//   let objCopy = {};
+
+//   let key;
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
+
+//   return objCopy;
+// }
+
+
+const numbers = {
+  a: 2, 
+  b: 5,
+  c: {
+    x: 11,
+    y: 6
+  }
+};
+
+const add = {
+  d: 14,
+  e: 11
+};
+
+// console.log(Object.assign(numbers, add));
+
+// const clone = (Object.assign({}, add));
+
+// console.log(add);
+// console.log(clone);
+
+// clone.d = 'CCC';
+
+// const newNumbers = copy(numbers);
+// newNumbers.a = 'БУКВА 2';
+
+// newNumbers.c.x = '111111';
+
+// console.log('ДО ', numbers);
+// console.log('Копия ', newNumbers);
+
+
+
+
+// clone.d = 20;
+// clone.e = 22;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'TRATARTR';
+
+// console.log(oldArray);
+// console.log(newArray);
+
+
+// const video = ['youtube', 'rutube', 'video-ok'],
+//       blog = ['wordpress', 'livejournal', 'blogger'],
+//       // internet = [];
+//       internet = [...video, ...blog, 'google', 'yandex', 'mail'];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// const api = [1, 5, 6];
+
+// log(...api);
+
+// const array = [1, 2];
+
+// const OBJ = {
+//   a: 1,
+//   b: 2
+// };
+
+// const newOBJ = {...OBJ};
+
+// console.log(newOBJ);
+
+
+
+
+
