2e1c1b3c4621b8580de5fe0c18b8de84657800a5
SkayPC <worldskaynet@gmail.com>
2021-04-01 16:25:14 +0700

    Local storage

---
+++ 24 Local Storage/script.js
@@ -0,0 +1,38 @@
+// 'use strict';
+
+// localStorage.setItem('number', 5); // Записать данные
+
+// // localStorage.removeItem('number'); // Удалить данные из локального хранилища
+// // localStorage.clear(); // Очищает значения 
+
+// console.log(localStorage.getItem('number')); // Получить данные
+
+// const checkbox = document.querySelector('#checkbox'),
+//   form = document.querySelector('form'),
+//   change = document.querySelector('#color');
+
+// if (localStorage.getItem('isChecked')) {
+//   checkbox.checked = true;
+// }
+
+// if (localStorage.getItem('bg') === 'changed') {
+//   form.style.backgroundColor = '#red';
+// }
+
+// checkbox.addEventListener('change', () => {
+//   localStorage.setItem('isChecked', true);
+// });
+
+// change.addEventListener('click', () => {
+//   if (localStorage.getItem('bg') === 'changed') {
+//     localStorage.removeItem('bg');
+//     form.style.backgroundColor = '#fff';
+//   } else {
+//     localStorage.setItem('bg', 'changed');
+//     form.style.backgroundColor = 'red';
+//   }
+// })
+
+
+
+
