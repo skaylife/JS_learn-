e2bc7000b615594e18ecf1c42332416904c90de8
SkayPC <worldskaynet@gmail.com>
2021-04-07 21:34:16 +0700

    Добавлен LocalStorage для калькулятора

--- 30 Калькулятор ккал/js/script.js
+++ 30 Калькулятор ккал/js/script.js
@@ -466,9 +466,43 @@ window.addEventListener('DOMContentLoaded', function () {
   // Калькулятор калорий
 
   const result = document.querySelector('.calculating__result');
-  let sex = 'female',
-    height, weight, age,
+
+  let sex, height, weight, age, ratio; // Переменные - пол, рост, вес возраст, и коэфф
+
+  // Условия для проверки значений из LocalStorage / Если они есть записать в LocalStorage, если нет, то поставить по умолчанию
+  if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  }
+
+  // Тоже самое только для коэффа - ratio
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+  } else {
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+  }
+
+  // Установка активных значений LocalStorage
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    // Проверка из localStorage
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) { // Если хоть одно значение пустое, тогда завершаем функцию 
@@ -485,15 +519,17 @@ window.addEventListener('DOMContentLoaded', function () {
 
   calcTotal();
 
-  function getStaticInformation(parentSelector, activeClass) {
-    const elements = document.querySelectorAll(`${parentSelector} div`); // Получаем все div-ы
+  function getStaticInformation(Selector, activeClass) {
+    const elements = document.querySelectorAll(Selector); // Получаем все div-ы
 
     elements.forEach(elem => {
       elem.addEventListener('click', (e) => {
         if (e.target.getAttribute('data-ratio')) {
           ratio = +e.target.getAttribute('data-ratio'); // Кнопки - Низкая активност Невысокая активность Умеренная активность 
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio')); // Сохраняем значение в LocalStorage
         } else {
           sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id')); // Сохраняем значение в LocalStorage
         }
 
         elements.forEach(elem => {
@@ -507,13 +543,21 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  getStaticInformation('#gender', 'calculating__choose-item_active');
-  getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+  getStaticInformation('#gender div', 'calculating__choose-item_active');
+  getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
   function getInputInformation(selector) {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+
+      // Подсветка рамки при вводе неправильных данных 
+      if (input.value.match(/\D/g)) {
+        input.style.border = '1px solid red';
+      } else {
+        input.style.border = '';
+      }
+
       switch (input.getAttribute('id')) {
         case 'height':
           height = +input.value;
