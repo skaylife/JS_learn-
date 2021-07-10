1118772b79effe8806ac477ab579b555332b63cf
SkayPC <worldskaynet@gmail.com>
2021-07-10 17:09:37 +0700

    init commit

---
+++ Php Connect React/lesson-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="constainer">
+      <h1>Dragon Info</h1>
+      <button>add</button>
+    </div>
+  );
+}
+
+export default App;
