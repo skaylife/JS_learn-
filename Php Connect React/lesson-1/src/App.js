6c8c86dbc885386f421135e0ed6581a5423b78f6
SkayPC <worldskaynet@gmail.com>
2021-07-10 18:03:21 +0700

    Connect php and react Вывод в консоли

--- Php Connect React/lesson-1/src/App.js
+++ Php Connect React/lesson-1/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEfect } from 'react';
 
 
 
 function App() {
+
+
+
   function clickHandler() {
     fetch("http://react-and-php", {
       method: 'POST',
-      headers: {
-        'Contetnt-Type': 'application/x-www-forum-urlencoded'
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
       },
       body: JSON.stringify({ action: 1 })
     })
       .then(response => response.text())
       .then(response => {
-        console.log("Work : ", response);
+        console.log(response);
       })
   }
 
