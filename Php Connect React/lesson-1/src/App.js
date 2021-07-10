68bc2a70c82faacabba6db4e651b88eb6cde8105
SkayPC <worldskaynet@gmail.com>
2021-07-10 17:20:30 +0700

    add fetch server

--- Php Connect React/lesson-1/src/App.js
+++ Php Connect React/lesson-1/src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  function clickHandler() {
+    fetch("http://react-and-php", {
+      method: 'POST',
+      headers: {
+        'Contetnt-Type': 'application/x-www-forum-urlencoded'
+      },
+      body: JSON.stringify({ action: 1 })
+    })
+      .then(response => response.text())
+      .then(response => {
+        console.log("Work : ", response);
+      })
+  }
+
   return (
     <div className="constainer">
       <h1>Dragon Info</h1>
-      <button>add</button>
+      <button onClick={clickHandler}>Go</button>
     </div>
   );
 }
 
 export default App;
+
