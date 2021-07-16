2fa012e8cba64638a61edd2c0a96725686ffd29f
SkayPC <worldskaynet@gmail.com>
2021-07-16 19:29:45 +0700

    basic work

--- #React/book-db-go/src/components/randomChar/randomChar.js
+++ #React/book-db-go/src/components/randomChar/randomChar.js
@@ -6,29 +6,60 @@ import ErrorMessage from '../error/errorMessage';
 
 
 export default class RandomChar extends Component {
+
+    constructor() {
+        super();
+        this.updateChar();
+    }
+
+    gotService = new gotService()
+
+    state = {
+        name: null,
+        gender: null,
+        born: null,
+        died: null,
+        culture: null
+    }
+
+    updateChar() {
+        const id = Math.floor(Math.random() * 140 + 25); // генератор от 25-140
+        this.gotService.getCharacter(id)
+            .then((char) => {
+                this.setState({
+                    name: char.name,
+                    gender: char.gender,
+                    born: char.born,
+                    died: char.died,
+                    culture: char.culture
+                })
+            });
+    }
+
     render() {
 
-
+        const { name, gender, born, died, culture } = this.state;
 
         return (
             <>
-                <h4>Random Character: John</h4>
+                <h5>Random Character:</h5>
+                <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Born </span>
-                        <span>11.31.1234</span>
+                        <span>{born}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Died </span>
-                        <span>13.21.1948</span>
+                        <span>{died}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </li>
                 </ul>
             </>
