70fe860cce892e8d178a45afa247485cb495a1d5
SkayPC <worldskaynet@gmail.com>
2021-07-16 18:25:07 +0700

    continue book-db-go

---
+++ #React/book-db-go/src/components/randomChar/randomChar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './randomChar.css';
+import gotService from '../../services/gotService';
+import Spinner from '../spinner/';
+import ErrorMessage from '../error/errorMessage';
+
+
+export default class RandomChar extends Component {
+
+    render() {
+
+        return (
+            <>
+                <h4>Random Character: John</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Gender </span>
+                        <span>male</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Born </span>
+                        <span>11.03.1555</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Died </span>
+                        <span>13.09.1088</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Culture </span>
+                        <span>Anarchy</span>
+                    </li>
+                </ul>
+            </>
+        )
+    }
+}
