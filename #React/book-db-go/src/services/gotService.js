34236b75eb9b654964400809b93d7f01f4f7f85e
SkayPC <worldskaynet@gmail.com>
2021-07-16 19:53:04 +0700

    ref code

--- #React/book-db-go/src/services/gotService.js
+++ #React/book-db-go/src/services/gotService.js
@@ -6,10 +6,10 @@ export default class GotService {
 
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`);
-    
+
         if (!res.ok) {
-          throw new Error(`Could not fetch ${url}` +
-            `, received ${res.status}`);
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`);
         }
         return await res.json();
     }
@@ -18,31 +18,34 @@ export default class GotService {
         const res = await this.getResource(`/books/`);
         return res.map(this._transformBook);
     }
-    
+
     async getBook(id) {
         const book = await this.getResource(`/books/${id}/`);
         return this._transformBook(book);
     }
-    
+
     async getAllCharacters() {
         const res = await this.getResource(`/characters?page=5&pageSize=10`);
         return res.map(this._transformCharacter);
     }
-    
+
     async getCharacter(id) {
         const character = await this.getResource(`/characters/${id}`);
         return this._transformCharacter(character);
     }
-    
+
     async getAllHouses() {
         const res = await this.getResource(`/houses/`);
         return res.map(this._transformHouse);
     }
-    
+
     async getHouse(id) {
         const house = this.getResource(`/houses/${id}/`);
         return this._transformHouse(house);
     }
+
+
+
 
     isSet(data) {
         if (data) {
@@ -56,7 +59,7 @@ export default class GotService {
             name: this.isSet(char.name),
             gender: this.isSet(char.gender),
             born: this.isSet(char.born),
-            died: this.isSet(char.died), 
+            died: this.isSet(char.died),
             culture: this.isSet(char.culture)
         };
     }
@@ -71,7 +74,7 @@ export default class GotService {
             ancestralWeapons: house.ancestralWeapons
         };
     }
-    
+
     _transformBook(book) {
         return {
             name: book.name,
