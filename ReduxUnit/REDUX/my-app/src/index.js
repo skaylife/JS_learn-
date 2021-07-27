3ab832011921ae012d7efe1e4df5c61dc954687c
SkayPC <worldskaynet@gmail.com>
2021-07-27 18:21:30 +0700

    DEC | INC

--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -1,27 +1,41 @@
 import { createStore } from 'redux';
 
-
-
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state + 1;
+    case 'DEC':
+      return state - 1;
     default:
       return state;
   }
 }
 
-const store = createStore(reducer)
-console.log(store.getState());
+const store = createStore(reducer);
 
-store.dispatch({ type: 'INC' });
-console.log(store.getState());
-store.dispatch({ type: 'INC' });
-console.log(store.getState());
-store.dispatch({ type: 'INC' });
-console.log(store.getState());
-store.dispatch({ type: 'INC' });
-console.log(store.getState());
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({ type: 'INC' })
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({ type: 'DEC' })
+});
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// })
+
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
+
+
 
 // let state = reducer(undefined, { type: 'INC' });
 
