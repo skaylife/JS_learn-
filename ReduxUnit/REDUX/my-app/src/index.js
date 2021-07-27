6fba25d55003f7f91143f837b1dc920e05ee3d39
SkayPC <worldskaynet@gmail.com>
2021-07-27 16:48:42 +0700

    Работа со Store и Reducer

--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -1,1 +1,31 @@
-console.log("Hello redux");
+import { createStore } from 'redux';
+
+
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
+console.log(store.getState());
+
+store.dispatch({ type: 'INC' });
+console.log(store.getState());
+store.dispatch({ type: 'INC' });
+console.log(store.getState());
+store.dispatch({ type: 'INC' });
+console.log(store.getState());
+store.dispatch({ type: 'INC' });
+console.log(store.getState());
+
+// let state = reducer(undefined, { type: 'INC' });
+
+// state = reducer(state, {});
+// console.log(state);
+// state = reducer(state, { type: 'INC' });
+// console.log(state);
