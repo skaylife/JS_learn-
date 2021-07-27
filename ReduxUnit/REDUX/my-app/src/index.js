c0d46ad90faa27fac9c45e292e6eb435d7d424a3
SkayPC <worldskaynet@gmail.com>
2021-07-27 18:48:40 +0700

    DEC INC RND

--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -6,20 +6,33 @@ const reducer = (state = 0, action) => {
       return state + 1;
     case 'DEC':
       return state - 1;
+    case 'RND':
+      return state + action.value;
     default:
       return state;
   }
 }
 
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = (value) => ({ type: 'RND', value });
+
 const store = createStore(reducer);
 
 document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch({ type: 'INC' })
+  store.dispatch(inc())
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch({ type: 'DEC' })
+  store.dispatch(dec())
 });
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+});
+
+
 
 const update = () => {
   document.getElementById('counter').textContent = store.getState();
