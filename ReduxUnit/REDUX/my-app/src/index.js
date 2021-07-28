1d23d3ed467083d969c5d1caeb8232226fdc83ff
SkayPC <worldskaynet@gmail.com>
2021-07-28 20:15:52 +0700

    ref add new file

--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -1,21 +1,8 @@
 import { createStore } from 'redux';
 
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'RND':
-      return state + action.value;
-    default:
-      return state;
-  }
-}
 
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (value) => ({ type: 'RND', value });
+
+
 
 const store = createStore(reducer);
 
