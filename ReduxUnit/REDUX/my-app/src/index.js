1fbe31c8135763ab127cf68795d7c7ed2947b9f9
SkayPC <worldskaynet@gmail.com>
2021-07-28 23:04:37 +0700

    ref simple code

--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -1,25 +1,19 @@
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
 
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-const { incDispatch, decDispatch, rndDispatch } = bindActionCreators(
-  {
-    incDispatch: inc,
-    decDispatch: dec,
-    rndDispatch: rnd
-  }, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
-document.getElementById('dec').addEventListener('click', decDispatch);
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value)
+  rnd(value)
 });
 
 
