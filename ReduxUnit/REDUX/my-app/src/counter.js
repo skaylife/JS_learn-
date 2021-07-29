45b9d23ec4b68697821b4cd67fd976fcbbd6d600
SkayPC <worldskaynet@gmail.com>
2021-07-29 16:04:46 +0700

    fix bug

---
+++ ReduxUnit/REDUX/my-app/src/counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+  return (
+    <div className="jumbotron">
+      <h1 className="text-white mb-3">{counter}</h1>
+      <button onClick={inc} className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Dec</button>
+      <button onClick={dec} className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Inc</button>
+      <button onClick={rnd} className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Rnd</button>
+    </div>
+  )
+}
+
+export default Counter;
--- ReduxUnit/REDUX/my-app/src/index.js
+++ ReduxUnit/REDUX/my-app/src/index.js
@@ -1,6 +1,9 @@
 import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './counter';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 const store = createStore(reducer);
@@ -9,20 +12,25 @@ const { dispatch } = store;
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value)
-});
-
-
 
 const update = () => {
-  document.getElementById('counter').textContent = store.getState();
+
+
+  ReactDOM.render(<Counter
+    counter={store.getState()}
+    inc={inc}
+    dec={dec}
+    rnd={() => {
+      const value = Math.floor(Math.random() * 10);
+      rnd(value);
+    }} />, document.getElementById('root'));
 }
 
+update(); // Вызов функции 
+
 store.subscribe(update);
+
+
 
 // store.subscribe(() => {
 //   console.log(store.getState());
