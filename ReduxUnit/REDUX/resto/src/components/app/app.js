828764d6dec341c20168e94abcdf0ed526bafdb4
SkayPC <worldskaynet@gmail.com>
2021-07-29 21:53:25 +0700

    Создание базового каркаса

--- ReduxUnit/REDUX/resto/src/components/app/app.js
+++ ReduxUnit/REDUX/resto/src/components/app/app.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
-
+import WithRestoService from '../hoc/with-resto-service';
 import Background from './food-bg.jpg';
 
-const App = () => {
+const App = ({ RestoService }) => {
+    console.log(RestoService.url)
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+            <AppHeader total={50} />
+            <MainPage />
+            <CartPage />
         </div>
     )
 }
 
-export default App;
+export default WithRestoService()(App);
